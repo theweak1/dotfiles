a3431cf4a7cad32d2b8553c0c98c357897f2c5af
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-08-26 16:28:05 -0400

    Fix issue with installing fisher.

--- fish_install.bash
+++ fish_install.bash
@@ -17,14 +17,11 @@ fi
 
 echo "checking if fisher is installed..."
 if [ -d ~/.config/fish/functions/fisher.fish ] || [ -d ~/.config/fish/completions/fisher.fish ]; then
-  echo "fisher is already installed."
-else
   echo "Installing fisher..."
-  # Remove conflicting files
-  rm -f ~/.config/fish/functions/fisher.fish ~/.config/fish/completions/fisher.fish
-  # Install fisher
   fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
   echo "fisher installed successfully."
+else
+  echo "fisher is already installed."
 fi
 
 echo "checking if tide is installed..."
@@ -35,6 +32,5 @@ else
   fish -c "fisher install IlanCosman/tide@v5"
 fi
 
-rm -f ~/.config/fish/fish_variables
 echo "fish_install.bash finished."
 
