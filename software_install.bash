7285dd5a584cad3e603cfe90d36e42b51902b0a6
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-09-08 00:52:30 -0400

    Updated script to download bat from release package and install it as well. Additionally added to install exa in this same script.

--- software_install.bash
+++ software_install.bash
@@ -4,9 +4,13 @@ echo -e "\n<<< Starting $0 >>>\n"
 
 #check if bat is installed
 echo -e "\n<<< Checking if bat is installed. >>>\n"
-if ! command -v "batcat" &> /dev/null; then
+if ! command -v "bat" &> /dev/null; then
   echo "bat is not installed. Installing..."
-  sudo apt install bat -y
+ wget -O ~/Downloads/bat-musl_0.23.0_amd64.deb https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-musl_0.23.0_amd64.deb 
+if [ -e ~/Downloads/bat-musl_0.23.0_amd64.deb ]; then
+  sudo dpkg -i ~/Downloads/bat-musl_0.23.0_amd64.deb
+  rm ~/Downloads/bat-musl_0.23.0_amd64.deb
+  fi
 else
   echo "bat is already installed."
 fi
@@ -18,6 +22,15 @@ if ! command -v "pip3" &> /dev/null; then
   sudo apt install python3-pip -y
 else
   echo "pip3 is already installed."
+fi
+
+#check if exa is installed
+echo -e "\n<<< Checking if exa is installed. >>>\n"
+if [ -x "$(command -v exa)" ]; then
+  echo "exa is already installed."
+else
+  echo "exa is not installed. Installing exa..."
+  sudo apt install exa -y
 fi
 
 echo -e "\n<<< software install finished. >>>\n"
