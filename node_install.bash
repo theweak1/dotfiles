ad3ea7cfe7daa1045e4b2ce311c2651757c2f0d6
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-09-09 01:31:01 -0400

    Add node_install.bash and matching Dotbot command
    Installs latest via n.

--- node_install.bash
+++ node_install.bash
@@ -2,14 +2,15 @@
 
 echo -e "\n<<< Running $0 >>>\n"
 
-# Check if n is installed
-echo -e "\n<<< Checking if n is installed. >>>\n"
-if ! command -v "n" &> /dev/null; then
-  echo "n is not installed. Installing..."
-  curl -L https://bit.ly/n-install | bash
-  mv $HOME/n $HOME/.n
+# Node versions are managed with `n`, which is can be installed using
+# the following command `curl -L https://bit.ly/n-install | N_PREFIX=$HOME/.n bash -s  -- -yqn`
+# See config.fish for N_PREFIX variable and addition to PATH.
+
+if ! command -v "node" &> /dev/null; then
+  echo "Installing Node & NPM with n..."
+  curl -L https://bit.ly/n-install | N_PREFIX=$HOME/.n bash -s  -- -yqn
 else
-  echo "n is already installed."
+  echo "node $(node --version) & NPM $(npm --version) already installed."
 fi
 
 echo -e "\n<<< node setup finished. >>>\n"
