8042a4d7e9fd75d0b31016d99c8eebc5bb708785
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-09-30 15:35:25 -0400

    Fix some bugs for the PATH export.
    Also refactor npm packages install in to its own script file.

--- scripts/node_install.bash
+++ scripts/node_install.bash
@@ -11,57 +11,28 @@ if ! command -v "node" &> /dev/null; then
   curl -L https://bit.ly/n-install | N_PREFIX=$HOME/.n bash -s  -- -yqn
 
 # This is needed for "n"
-export N_PREFIX="$HOME/.n"
+# Define a marker for the block of text
+marker="# N_PREFIX block"
 
-# Check if $N_PREFIX/bin is already in the PATH
-if [[ ":$PATH:" != *":$N_PREFIX/bin:"* ]]; then
-    # If not, add it to the PATH
-    export PATH="$N_PREFIX/bin:$PATH"
-    
-    # Define the lines to be added to .bashrc
-    lines='
-export N_PREFIX="$HOME/.n"
-if [[ ":$PATH:" != *":$N_PREFIX/bin:"* ]]; then
-    export PATH="$N_PREFIX/bin:$PATH"
+# Define the lines to be added to .bashrc
+lines="
+$marker
+export N_PREFIX=\"$HOME/.n\"
+if [[ \":\$PATH:\" != *\":\$N_PREFIX/bin:\"* ]]; then
+    export PATH=\"\$N_PREFIX/bin:\$PATH\"
 fi
-'
-    # Check if the lines are already present in .bashrc, if not, append them
-    if ! grep -q "$lines" ~/.bashrc ; then
-        echo "$lines" >> ~/.bashrc
-        echo "Lines were added to .bashrc"
-    else
-        echo "Lines are already present in .bashrc"
-    fi
+$marker
+"
+
+# Check if the marker is already present in .bashrc, if not, append the lines
+if ! grep -q "$marker" ~/.bashrc ; then
+    echo "$lines" >> ~/.bashrc
+    echo "Lines were added to .bashrc"
 else
-    echo "$N_PREFIX/bin is already in the PATH"
+    echo "Lines are already present in .bashrc"
 fi
 
 else
   echo "node $(node --version) & NPM $(npm --version) already installed."
 fi
 
-
-# Function to install a global npm package if it's not already installed
-install_global_package() {
-  local package_name="$1"
-  if ! npm ls -g "$package_name" &> /dev/null; then
-    echo "Installing $package_name..."
-    npm install --global "$package_name"
-  else
-    echo "$package_name is already installed."
-  fi
-}
-
-# Install Global NPM Packages with conditions
-install_global_package "npm@latest"
-install_global_package "commitizen"
-install_global_package "cz-conventional-changelog"
-install_global_package "pm2"
-install_global_package "typescript"
-install_global_package "trash-cli"
-install_global_package "vercel"
-install_global_package "eslint"
-install_global_package "@johnnymorganz/stylua-bin"
-
-echo "Global NPM Packages Installed:"
-npm list --global --depth=0
---
+++ scripts/npm_packages_install.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Function to install a global npm package if it's not already installed
+install_global_package() {
+  local package_name="$1"
+  if ! npm ls -g "$package_name" &> /dev/null; then
+    echo "Installing $package_name..."
+    npm install --global "$package_name"
+  else
+    echo "$package_name is already installed."
+  fi
+}
+
+# Install Global NPM Packages with conditions
+install_global_package "npm@latest"
+install_global_package "commitizen"
+install_global_package "cz-conventional-changelog"
+install_global_package "pm2"
+install_global_package "typescript"
+install_global_package "trash-cli"
+install_global_package "vercel"
+install_global_package "eslint"
+install_global_package "@johnnymorganz/stylua-bin"
+
+echo "Global NPM Packages Installed:"
+npm list --global --depth=0
