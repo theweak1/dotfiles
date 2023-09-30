848426620e94aeb57cb9b4dc44990f4a4532e8c6
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-09-30 15:47:01 -0400

    Add n/bin/ export PATH for config.fish

--- scripts/node_install.bash
+++ scripts/node_install.bash
@@ -11,26 +11,46 @@ if ! command -v "node" &> /dev/null; then
   curl -L https://bit.ly/n-install | N_PREFIX=$HOME/.n bash -s  -- -yqn
 
 # This is needed for "n"
+
 # Define a marker for the block of text
 marker="# N_PREFIX block"
 
 # Define the lines to be added to .bashrc
-lines="
+bash_lines="
 $marker
 export N_PREFIX=\"$HOME/.n\"
 if [[ \":\$PATH:\" != *\":\$N_PREFIX/bin:\"* ]]; then
-    export PATH=\"\$N_PREFIX/bin:\$PATH\"
+  export PATH=\"\$N_PREFIX/bin:\$PATH\"
 fi
 $marker
 "
 
-# Check if the marker is already present in .bashrc, if not, append the lines
-if ! grep -q "$marker" ~/.bashrc ; then
-    echo "$lines" >> ~/.bashrc
-    echo "Lines were added to .bashrc"
-else
-    echo "Lines are already present in .bashrc"
-fi
+# Define the lines to be added to config.fish
+fish_lines="
+$marker
+set -x N_PREFIX \"$HOME/.n\"; contains \"$N_PREFIX/bin\" \$PATH; or set -a PATH \"$N_PREFIX/bin\"
+$marker
+"
+
+# Function to append lines to a file if the marker is not present
+append_lines_if_not_present() {
+  local lines="$1"
+  local file="$2"
+
+  if ! grep -q "$marker" "$file"; then
+    echo "$lines" >> "$file"
+    echo "Lines were added to $file"
+  else
+    echo "Lines are already present in $file"
+  fi
+}
+
+# Add lines to .bashrc
+append_lines_if_not_present "$bash_lines" ~/.bashrc
+
+# Add lines to config.fish
+append_lines_if_not_present "$fish_lines" ~/.config/fish/config.fish
+
 
 else
   echo "node $(node --version) & NPM $(npm --version) already installed."
