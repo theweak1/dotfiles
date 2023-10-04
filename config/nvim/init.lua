de6f45267f0bdf0833e11ffb5ea13abc991bc141
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-10-04 17:59:43 -0400

    Update neo-tree configs to always show dotfiles, and to hide node_modules.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -189,7 +189,17 @@ require('lazy').setup({
     version = '*',
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', 'MunifTanjim/nui.nvim' },
     config = function()
-      require('neo-tree').setup {}
+      require('neo-tree').setup({
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {
+              "node_modules"
+            },
+          },
+        }
+      })
     end,
   },
 
