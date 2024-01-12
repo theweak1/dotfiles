7908ba60dbbd0486770c08002633bf130bda7002
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-01-12 19:51:29 -0400

    Updated keymaps.

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -1,51 +1,53 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          --lsp
-          "bashls",
-          "cssls",
-          "eslint",
-          "gopls",
-          "html",
-          "jsonls",
-          "lua_ls",
-          "marksman",
-          --"prismals",
-          --"tailwindcss",
-          "tsserver",
-        },
-      })
-    end,
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					--lsp
+					"bashls",
+					"cssls",
+					"eslint",
+					"gopls",
+					"html",
+					"jsonls",
+					"lua_ls",
+					"marksman",
+					--"prismals",
+					--"tailwindcss",
+					"tsserver",
+				},
+			})
+		end,
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "info" })
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "go to definition" })
-      vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "go to declaration" })
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
-    end,
-  },
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "info" })
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "rename" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "code actions" })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "go to definition" })
+			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "go to declaration" })
+			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "go to implementation" })
+		end,
+	},
 }
