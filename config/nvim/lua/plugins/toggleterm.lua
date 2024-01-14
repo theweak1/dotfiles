6ee874728f7504831c754e5647261e3b08da15df
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-01-13 23:02:34 -0400

    Fixed keymap.

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("toggleterm").setup({
 			size = 20,
-			open_mapping = [[<c-\>]],
+			open_mapping = [[<C-\>]],
 			hide_numbers = true,
 			shade_filetypes = {},
 			shade_terminals = true,
