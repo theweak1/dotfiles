5f883a20933e4100825d82ffac8bd4f3ae019359
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-01-14 18:00:59 -0400

    Added keymap to search `todo-comments` with telescope.

--- config/nvim/lua/plugins/todo-comments.lua
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -1,7 +1,8 @@
 return {
-    "folke/todo-comments.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-    config = function()
-        require("todo-comments").setup {}
-    end
+	"folke/todo-comments.nvim",
+	dependencies = "nvim-lua/plenary.nvim",
+	config = function()
+		require("todo-comments").setup({})
+		vim.keymap.set("n", "tt", "<cmd>:TodoTelescope<CR>", { desc = "Show todo comment in telescope" })
+	end,
 }
