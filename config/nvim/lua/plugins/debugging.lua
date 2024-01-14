9438038008e5a75f81f3f015e7284a99b3da2396
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-01-13 22:55:06 -0400

    Added debugger.

---
+++ config/nvim/lua/plugins/debugging.lua
@@ -0,0 +1,29 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "leoluz/nvim-dap-go",
+  },
+  config = function()
+    local dap, dapui = require("dap"), require("dapui")
+
+    require("dapui").setup()
+    require("dap-go").setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Toggle breakpoin" })
+    vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Debugger continue" })
+  end,
+}
