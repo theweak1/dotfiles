10d6be855b0551b73a14226e116c4547f8c2dd5e
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-01-30 20:41:35 -0400

    Fixed setup of signs options and added keymaps.

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,24 +1,32 @@
 return {
-	-- Adds git releated signs to the gutter, as well as utilities for managing changes
-	"lewis6991/gitsigns.nvim",
-	opts = {
-		-- See `:help gitsigns.txt`
-		signs = {
-			add = {
-				text = "+",
-			},
-			change = {
-				text = "~",
-			},
-			delete = {
-				text = "_",
-			},
-			topdelete = {
-				text = "‾",
-			},
-			changedelete = {
-				text = "~",
-			},
-		},
-	},
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup({
+      signs = {
+        add = {
+          text = "+",
+        },
+        change = {
+          text = "~",
+        },
+        delete = {
+          text = "_",
+        },
+        topdelete = {
+          text = "‾",
+        },
+        changedelete = {
+          text = "~",
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview git hunk" })
+    vim.keymap.set(
+      "n",
+      "<leader>gt",
+      ":Gitsigns toggle_current_line_blame<CR>",
+      { desc = "Show current line blame" }
+    )
+  end,
 }
