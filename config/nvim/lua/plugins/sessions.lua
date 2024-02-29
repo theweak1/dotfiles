91eeaa40ea55d7b9104cb417cc44aca70f65bf1f
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-02-29 18:16:50 -0400

    Added auto-session plugin

---
+++ config/nvim/lua/plugins/sessions.lua
@@ -0,0 +1,23 @@
+return {
+	"rmagatti/auto-session",
+	config = function()
+		require("auto-session").setup({
+			log_level = "error",
+			auto_session_suppress_dirs = { "~/", "~/.dotfiles/**", "~/Projects", "~/Downloads", "/" },
+		})
+		session_lens = {
+			buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+			load_on_setup = true,
+			theme_conf = { border = true },
+			previewer = false,
+		}
+		vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+			noremap = true,
+			desc = "Show all auto sessions",
+		})
+		vim.keymap.set("n", "<leader>ds", "<cmd> Autosession delete<CR>", {
+			noremap = true,
+			desc = "delete a auto sessions",
+		})
+	end,
+}
