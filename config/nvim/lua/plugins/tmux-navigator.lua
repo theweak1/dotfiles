5c98818c64713296f045dc46b83878654f3490c1
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-03-27 03:20:49 -0400

    Added vim-test plugin, plus added keymaps to navigate between tmux and nvim.

--- config/nvim/lua/plugins/tmux-navigator.lua
+++ config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,3 +1,8 @@
 return {
-	"christoomey/vim-tmux-navigator",
+  "christoomey/vim-tmux-navigator",
+
+  vim.keymap.set('n', 'C-h', ':TmuxNagiateLeft<CR>'),
+  vim.keymap.set('n', 'C-j', ':TmuxNagiateDown<CR>'),
+  vim.keymap.set('n', 'C-k', ':TmuxNagiateUp<CR>'),
+  vim.keymap.set('n', 'C-l', ':TmuxNagiateRight<CR>')
 }
