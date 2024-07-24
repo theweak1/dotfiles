25e727677d0bbee94213a284c2f5892767a713f3
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-07-24 14:51:16 -0400

    changed keymaps for bufferline

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -40,7 +40,7 @@ return {
 			},
 		})
 
-		vim.keymap.set("n", "<M-n>", "<Cmd>BufferLineCycleNext<CR>", {})
-		vim.keymap.set("n", "<M-p>", "<Cmd>BufferLineCyclePrev<CR>", {})
+		vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
+		vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
 	end,
 }
